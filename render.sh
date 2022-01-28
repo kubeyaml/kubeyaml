8010015ef1c8039fcfe7bee4a17f160d8116da66
jmnote <opcore@gmail.com>
2022-01-28 05:27:42 +0000

    .

--- render.sh
+++ render.sh
@@ -5,14 +5,26 @@ ROOT=$(pwd)
 
 cd kustomizations/
 YAMLS=$(ls *.yaml)
+
 cd ..
 select YAML in $YAMLS; do
 NAME=$(basename $YAML .yaml)
 rm -rf   _temp
 mkdir -p _temp/_rendered
 
-cp $ROOT/kustomizations/$YAML   _temp/kustomization.yaml
-kubectl kustomize --enable-helm _temp/ -o _temp/_rendered/
+cd _temp
+cp $ROOT/kustomizations/$YAML kustomization.yaml
+kubectl kustomize --enable-helm . -o _rendered/
+
+cd _rendered/
+FILES=$(ls *.yaml)
+for FILE in $FILES; do
+    KIND=$(grep ^kind: $FILE | awk '{print tolower($2)}')
+    NEWFILE=$(echo $FILE | grep -o $KIND.*)
+    mv $FILE $NEWFILE
+done
+
+cd $ROOT
 rm -rf             yamls/$NAME
 mv _temp/_rendered yamls/$NAME
 
